e45796fdfbc3a4b19625eecff19f3b2c09e25d93
Daniel Antonio <dernlsenpai@gmail.com>
2020-09-28 15:49:31 -0700

    Redid all the Errors

--- index.js
+++ index.js
@@ -7,27 +7,47 @@ server.use(express.json());
 const users = [{ id: 1, name: "Daniel Antonio", bio: "When's the next rave boiz?" }];
 
 server.get('/api/users', (req,res) => {
-    res.status(200).json(users);
+    try {
+        res.status(200).json(users);
+    } catch {
+        { errorMessage: "The users information could not be retrieved." }
+    }
 });
 
 server.get('/api/users/:id', (req,res) => {
     const id = parseInt(req.params.id);
     const user = users.find(u => u.id === id);
 
-    res.status(200).json(user);
+    if(!user) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." });
+    } else {
+        res.status(200).json(user);
+    }
 });
 
 server.post('/api/users', (req,res) => {
     let nextID = users.length;
     const newUser = { id: nextID + 1, ...req.body };
-    users.push(newUser);
 
-    res.status(201).json(users);
+    if(!req.body.name || !req.body.bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    }
+    
+    try {
+        users.push(newUser);
+        res.status(201).json(users);
+    } catch {
+        res.status(500).json({ errorMessage: "The user information could not be retrieved." })
+    }
 });
 
 server.delete('/api/users/:id', (req,res) => {
     const id = parseInt(req.params.id);
     const userIndex = users.findIndex(u => u.id === id);
+
+    if(userIndex < 0) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    }
 
     if(userIndex >= 0) {
         users.splice(userIndex, 1);
@@ -41,10 +61,21 @@ server.delete('/api/users/:id', (req,res) => {
 server.put('/api/users/:id', (req,res) => {
     const user = users.find(obj => obj.id === parseInt(req.params.id));
     const update = req.body;
-
     Object.assign(user, update);
 
-    res.status(202).json(user);
+    if(!req.body.name || !req.body.bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    }
+
+    if(!user) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." });
+    }
+
+    try {
+        res.status(200).json(update);
+    } catch {
+        res.status(500).json({ errorMessage: "The user information could not be modified." });
+    }
 })
 
 server.listen(port, () => console.log(`Server listening on port: ${port}....`));
