0836f6098cdbc2bf17f643350195b3e859953b31
Daniel Antonio <dernlsenpai@gmail.com>
2020-09-28 13:02:47 -0700

    Created GET request for individual user

--- index.js
+++ index.js
@@ -11,4 +11,11 @@ server.get('/api/users', (req,res) => {
     res.status(200).json(users);
 });
 
+server.get('/api/users/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+
+    res.status(200).json(user);
+});
+
 server.listen(port, () => console.log(`Server listening on port: ${port}....`));
