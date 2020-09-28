f6bf86d097e89d144b577f5ca067161d7f491767
Daniel Antonio <dernlsenpai@gmail.com>
2020-09-28 13:43:57 -0700

    Created DELETE user endpoint

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = 5000;
 server.use(express.json());
 
 const users = [{ id: 1, name: "Daniel Antonio", bio: "When's the next rave boiz?" }];
-let nextID = users.length;
 
 server.get('/api/users', (req,res) => {
     res.status(200).json(users);
@@ -19,10 +18,24 @@ server.get('/api/users/:id', (req,res) => {
 });
 
 server.post('/api/users', (req,res) => {
+    let nextID = users.length;
     const newUser = { id: nextID + 1, ...req.body };
     users.push(newUser);
 
     res.status(201).json(users);
+});
+
+server.delete('/api/users/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const userIndex = users.findIndex(u => u.id === id);
+
+    if(userIndex >= 0) {
+        users.splice(userIndex, 1);
+        res.status(202).json(users);
+    } else {
+        res.status(404).json({ message: 'ERROR: No User Found!' })
+    }
+
 });
 
 server.listen(port, () => console.log(`Server listening on port: ${port}....`));
